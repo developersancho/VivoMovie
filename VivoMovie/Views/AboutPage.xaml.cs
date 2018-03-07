58024d9c0847a37dd1883986eeca8208f3af2046
sancho <developersancho@gmail.com>
2018-03-07 21:55:39 +0300

    ***
    ***

---
+++ VivoMovie/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace VivoMovie
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
